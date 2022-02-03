99d4e21f985bb8912ac32695a4bd3dec28be6ba2
utkarsh <utkarsh@cyberfear.com>
2022-02-03 14:16:53 +1100

    🪐added download function

--- lib_track.py
+++ lib_track.py
@@ -13,3 +13,9 @@ class Track:
     self.artist = str()
     self.yt_hash = str()
 
+  #! pass generated youtube link to youtube-dl
+  def save_track(self):
+    to_save = f"https://www.youtube.com/watch?v={self.yt_hash}"
+    print(f"Downloading {self.index}. {self.title} by {self.artist} from the album: {self.album}")
+    os.system(f"youtube-dl -f bestaudio {to_save}")
+
